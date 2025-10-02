583ad053359844253be8c748f525e47f88ea8ba7
elian muriel <elian_2546@hotmail.com>
2025-10-01 19:02:37 -0500

    Primera clase 01/10/2025

---
+++ modulo_1/git/aap2.py
@@ -0,0 +1,9 @@
+print ('Bienvenidos PROGRAMACIÓN IV')
+
+print ('PROGRAMACIÓN IV')
+print ('PROGRAMACIÓN IV')
+print ('PROGRAMACIÓN IV')
+print ('Rama Main')
+if (4>5):
+    print (" 4 es mayor que 5")
+    
---
+++ modulo_1/git/app.py
@@ -0,0 +1,9 @@
+print ('Bienvenidos al 4to. semestre')
+
+print ('Rama Main')
+print ('Rama Main')
+print ('Rama Main')
+print ('Rama Main')
+if (4>5):
+    print (" 4 es mayor que 5")
+    
