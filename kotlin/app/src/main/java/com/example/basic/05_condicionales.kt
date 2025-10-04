b6f4fe3ed1c063e3e91bb78ad21c748056eaa252
elian muriel <elian_2546@hotmail.com>
2025-10-04 10:25:59 -0500

    Clase 04/10/2025

--- kotlin/app/src/main/java/com/example/basic/05_condicionales.kt
+++ kotlin/app/src/main/java/com/example/basic/05_condicionales.kt
@@ -20,4 +20,39 @@ fun main(){
     } else {
         println("Padawan")
     }
+
+    var tipoDroide: String = "BB-8"
+    when(tipoDroide){
+        "R2-D2", "R2-Q5"->println("Droide astromecanico")
+        "C-3PO", "C-3PA"->println("Droide de protocolo")
+        "BB-8", "BB-9E"->println("Droide de nueva generacion")
+        else->println("Modelo Desconocido")
+    }
+
+    var peligro: Int = 10
+    var recompensa: Int = 50
+    when{
+        peligro>8 && recompensa< 1000 ->println("Misión Rechazada")
+        peligro<=3 ->println("Misión Aceptada")
+        else->println("Requiere Evaluación Adicional")
+    }
+
+    var colorSemaforo: String = "Azul"
+    var estadoCalle: String = "Trafico"
+    when{
+        colorSemaforo == "Verde"->println("Avanzar")
+        colorSemaforo == "Amarillo" && estadoCalle == "Vacio" ->println("Avanzar con precaucion")
+        colorSemaforo == "Amarillo" && estadoCalle == "Trafico" ->println("Esperar")
+        colorSemaforo == "Rojo"->println("Esperar")
+        else->println("Semaforo dañado")
+    }
+
+    var mesNacimiento: String = ""
+    when(mesNacimiento){
+        "enero", "mayo", "septiembre"->println("Aire")
+        "febrero", "junio", "octubre"->println("Agua")
+        "marzo", "julio", "noviembre"->println("Fuego")
+        "abril", "agosto", "diciembre"->println("Tierra")
+        else->println("Mal escrito")
+    }
 }
