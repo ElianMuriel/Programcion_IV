479769abebc0bec7bb866f3dc4816a7a58b22304
elian muriel <elian_2546@hotmail.com>
2025-10-13 21:55:11 -0500

    clase 13/10/2025

---
+++ modulo_1/dart/02_variables.dart
@@ -0,0 +1,31 @@
+void main() {
+  print("Tipos de Variables en Dart");
+
+  final String pokemon = "Ditto";
+  print(pokemon);
+
+  String myName = 'Elian';
+  myName = 'Muriel';
+  print(myName);
+
+  const String elemento = "Fuego";
+  print("elemento: $elemento");
+  
+  bool active = false;
+  active = false;
+  print("Es activo: $active");
+  int hp = 1000;
+  print("Caballos de fuerza: $hp");
+  List<String> abilities =["impostor","Correlón"];
+  print("Habilidades $abilities");
+  final sprites = <String>["src/image1.jpg","image2.jpg"];
+  print("Imagenes: $sprites");
+  
+  print("Impresion en varias lineas");
+  print("""
+  $pokemon
+  $hp
+  $sprites
+  $abilities
+  """);
+}
