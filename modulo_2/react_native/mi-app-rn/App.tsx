5a87c86af0407e7413ccabeae903359c2a3ba710
elian muriel <elian_2546@hotmail.com>
2025-10-27 21:58:34 -0500

    Clase 27/10/2025

---
+++ modulo_2/react_native/mi-app-rn/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Saludo from "./src/components/saludo";
+import SaludoProps from "./src/components/SaludoProps";
+import Contador from './src/components/Contador';
+import Caja from './src/components/Caja';
+import Perfil from './src/components/Perfil';
+
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#0d1117" }}>
+      <Saludo />
+      <Text style={{ color: "yellow" }}>Los poderosos programadores del 4to. Semestre</Text>
+      <SaludoProps nombre="Elian Muriel" />
+      <Contador/>
+      <Caja/>
+      <Perfil/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
